8bf2ab4edc6c3acf081c08823ba596e402437426
Woosung Joung <mung3477@naver.com>
2024-07-02 21:58:08 +0900

    feat: Implemented hopfield network (WIP)

---
+++ hopfield_network/hopfield_network.py
@@ -0,0 +1,51 @@
+"""
+	Python implementation of the [Hopfield Network]
+		: (https://www.ncbi.nlm.nih.gov/pmc/articles/PMC346238/pdf/pnas00447-0135.pdf)
+	This code is slightly modified from
+			https://towardsdatascience.com/hopfield-networks-neural-memory-machines-4c94be821073
+"""
+
+import numpy as np;
+
+class HopfieldNetwork:
+	def __init__(self, input: np.ndarray) -> None:
+
+		if input.ndim == 2:
+			self.memories = input;
+			self.n = self.memories.shape[1];
+		elif input.ndim == 1:
+			self.memories = np.array([input])
+			self.n = input.shape[0];
+		else:
+			raise ValueError('Input must be an flatten image or a list of flatten images.');
+
+		# Use bipolarized states rather than bipolarizing binary states later
+		self.state = np.random.randint(-2, 2, (self.n, 1))
+		self.connections = np.zeros((self.n, self.n))
+		self.energy_logs = list()
+
+	def learn(self) -> None:
+		# Hebbian learning rule to memorize patterns
+		for pattern in self.memories:
+			self.connections += (1 / self.memories.shape[0]) * pattern.T @ pattern
+			np.fill_diagonal(self.connections, 0)
+
+	def update_states(self, W)->None:
+		# Generate random numbers for each neuron
+		attempt_probs = np.random.random(self.n)
+
+		# Determine which neurons attempt an update
+		# Each neuron i readjusts its state randomly in time but with a mean attempt rate W,
+		update_mask = attempt_probs < W
+		for neuron in np.nonzero(update_mask)[0]:
+			activation = np.dot(self.connections[neuron, :], self.state)
+			if activation < 0:
+				self.state[neuron] = -1
+			else:
+				self.state[neuron] = 1
+
+	def compute_energy(self)->None:
+		# Compute the energy of the current state
+		energy = -0.5 * np.dot(self.state.T, self.connections @ self.state)
+		self.energy_logs.append(energy)
+
